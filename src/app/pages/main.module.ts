bcee557f1bf7d0b3c2ec52134a8ac8e2eea44092
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-03-04 21:36:38 +0300

    Modules arranged.

---
+++ src/app/pages/main.module.ts
@@ -0,0 +1,31 @@
+import { MainNavComponent } from "./../nav/main-nav/main-nav.component";
+import { MainLayoutComponent } from "./../layout/main-layout/main-layout.component";
+
+import { ContactComponent } from "./contact/contact.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { HomeComponent } from "./home/home.component";
+
+import { AppRoutingModule } from "../app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { ComponentsModule } from "./../components/components.module";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutMeComponent,
+    ContactComponent,
+    MainLayoutComponent,
+    MainNavComponent,
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+})
+export class MainModule {}
