bfd65f81affdd2195059833511dc2cf844683022
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-03 23:48:04 +0300

    Admin module was created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
+import { AdminModule } from "./admin-pages/admin.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AdminLayoutComponent } from "./layout/admin-layout/admin-layout.component";
-import { AdminNavComponent } from "./nav/admin-nav/admin-nav.component";
-
 @NgModule({
-  declarations: [AppComponent, AdminLayoutComponent, AdminNavComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MainModule,
+    AdminModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
