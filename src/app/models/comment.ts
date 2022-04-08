d39eb1b5f91d1e1e3fb7f71fb8574947a37e62f2
Egemen Bahtiyar <e.bahtiyar51@gmail.com>
2022-04-09 01:42:58 +0300

    Comment model and service were created.

---
+++ src/app/models/comment.ts
@@ -0,0 +1,7 @@
+export class Comment {
+  id: number;
+  name: string;
+  contentMaim: string;
+  publishDate: Date;
+  aritcleId: number;
+}
---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Comment } from "./../models/comment";
+import { Injectable } from "@angular/core";
+import { tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CommentService {
+  constructor(private httpClient: HttpClient) {}
+  private apiUrl = "https://localhost:44332/api/Comments";
+  loading = false;
+
+  addComment(comment: Comment) {
+    this.loading = true;
+    return this.httpClient.post(this.apiUrl, comment).pipe(
+      tap((x) => {
+        this.loading = false;
+      })
+    );
+  }
+
+  commentList(id: number) {
+    return this.httpClient.get<Comment[]>(`${this.apiUrl}/${id}`);
+  }
+}
